64acc93118f2d0abe134ade59243db108281d979
Romashka <rroma13579@gmail.com>
2022-09-10 23:59:25 +0200

    second commit

--- js/script.js
+++ js/script.js
@@ -3,8 +3,16 @@ var blocks = document.querySelectorAll(".row-block");
 blocks.forEach(block => {
     block.addEventListener('click', (e) => {
         e.preventDefault();
+        var block_styles = block.getBoundingClientRect();
         var x = e.offsetX;
         var y = e.offsetY;
-        console.log(x, y);
+        console.log(x, y, Math.round(block_styles.width));
+        block.addEventListener('mousemove', (e) => {
+            var x1 = e.clientX;
+            var y1 = e.clientY;
+            block.style.position = "absolute"
+            block.style.left = ""+(x1-x)+"px"
+            block.style.top = ""+(y1-y)+"px"
+        });
     });
 });
