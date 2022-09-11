728c3a0d6e43d29e09e6d3a6eada01104a14b3c4
Romashka <rroma13579@gmail.com>
2022-09-11 13:58:03 +0200

    third commit

--- js/script.js
+++ js/script.js
@@ -1,16 +1,20 @@
 var blocks = document.querySelectorAll(".row-block");
+var pressed = false;
 
 blocks.forEach(block => {
-    block.addEventListener('click', (e) => {
+    block.addEventListener('mousedown', (e) => {
+        pressed = true;
         e.preventDefault();
         var block_styles = block.getBoundingClientRect();
         var x = e.offsetX;
         var y = e.offsetY;
+        block.style.cursor = 'grabbing'
         console.log(x, y, Math.round(block_styles.width));
         block.addEventListener('mousemove', (e) => {
             var x1 = e.clientX;
             var y1 = e.clientY;
             block.style.position = "absolute"
+            block.style.background = "rgb(140, 140, 140)"
             block.style.left = ""+(x1-x)+"px"
             block.style.top = ""+(y1-y)+"px"
         });
